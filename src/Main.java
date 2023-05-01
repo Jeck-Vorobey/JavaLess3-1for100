4012ecef1e39c01925ad597f017d5d3a1b3b1371
Evgeni <ewgeny11@mail.ru>
2023-05-01 19:28:24 +0300

    revision commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        int beg = 695;          //начальная сумма на счете
-        int plus = 1120;        //сумма пополнения
+        int beg = 700;          //начальная сумма на счете
+        int plus = 1099;        //сумма пополнения
+        int bonus = 0;
+
 
         if (plus > 1000) {
-            plus = plus + (plus - 1000) / 100;
-        }
-        int end = beg + plus;       //итоговая сумма с учетом бонусов
+            bonus = (plus - 1000) / 100;
+            if (bonus > 0) {
+                System.out.println(" Поздравляем! Вам начислены бонусы в размере " + bonus + " руб. ");
+            }
 
-        System.out.println(end);
+        }
+        int end = beg + plus + bonus;       //итоговая сумма с учетом бонусов
+
+        System.out.println(" Сумма на Вашем счёте " + end + " руб. ");
+
     }
 }
